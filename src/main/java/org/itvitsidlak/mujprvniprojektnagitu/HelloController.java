ede4754e074c786683951380d86dde8165eabc07
vitulinka <vitek.sidlak@gmail.com>
2024-06-18 09:49:49 +0200

    funguje menění velikosti kol :)

--- src/main/java/org/itvitsidlak/mujprvniprojektnagitu/HelloController.java
+++ src/main/java/org/itvitsidlak/mujprvniprojektnagitu/HelloController.java
@@ -2,13 +2,20 @@ package org.itvitsidlak.mujprvniprojektnagitu;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.control.ColorPicker;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable{
     @FXML
     private Polygon auto2;
     @FXML
@@ -25,6 +32,8 @@ public class HelloController {
     private Circle kolo2;
     @FXML
     private ColorPicker color;
+    @FXML
+    private Slider posouvac;
 
     public boolean wracked = false;
 
@@ -32,5 +41,16 @@ public class HelloController {
         auto1.setFill(color.getValue());
         auto2.setFill(color.getValue());
         auto3.setFill(color.getValue());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        posouvac.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                kolo1.setRadius((double)t1);
+                kolo2.setRadius((double)t1);
+            }
+        });
     }
 }
